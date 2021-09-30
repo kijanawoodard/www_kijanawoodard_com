19d889d94547dd76270e2f13fe6302b4f96e8c9c
Kijana Woodard <kijana@kijanawoodard.com>
2021-09-30 07:27:16 -0500

    add just enough for netlify

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 build: 
 	dotnet build .\src\www_kijana_woodard_com.sln.sln
+run: 
+	dotnet run --project .\src\Engine\Engine.csproj src\site
--- src/Engine/Program.cs
+++ src/Engine/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var path = args[0];
+var filename = Path.Combine(path, "index.html");
+Console.WriteLine($"Hello, World!\n\n{filename}");
+Directory.CreateDirectory(path);
+File.WriteAllText(filename, "Hello World!");
